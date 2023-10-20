0f7c589785d974ac2502e0ea712d0ffff1a02d40
jared-paquette <jared.paquette@baycollege.edu>
2023-10-20 14:07:14 -0400

    Add datadog logging and delete crontab

--- test_template/app.py
+++ test_template/app.py
@@ -1,10 +1,10 @@
 import logging
 import os
 
-import logging_loki
 from dotenv import find_dotenv, load_dotenv
 
 from .email_functions.email import EmailUsers
+from .logging.datadog_logger import logger
 from .math_functions.basic_functions import add, div, mult, sub
 
 
@@ -31,27 +31,9 @@ class TestTemplate:
         DIV_1 = int(os.environ.get('DIV_1'))
         DIV_2 = int(os.environ.get('DIV_2'))
 
-        GRAFANA_URL = os.environ.get('GRAFANA_URL')
-        GRAFANA_USERNAME = os.environ.get('GRAFANA_USERNAME')
-        GRAFANA_PASSWORD = os.environ.get('GRAFANA_PASSWORD')
-
         # *************************************** #
         # *           SETUP LOGGING             * #
         # *************************************** #
-
-        handler = logging_loki.LokiHandler(
-            url=GRAFANA_URL,
-            tags={"application": "test_template"},
-            auth=(GRAFANA_USERNAME, GRAFANA_PASSWORD),
-            version="1",
-        )
-
-        logger = logging.getLogger("test-template-logger")
-        logger.addHandler(handler)
-        logger.level = logging.INFO
-
-        logger.info("Testing Our Template Repo",
-                    extra={"tags": {"service": "main"}})
 
         # **************************************** #
         # *            MATH OPERATIONS           * #
@@ -65,19 +47,18 @@ class TestTemplate:
             div_result = div(DIV_1, DIV_2)
         except ZeroDivisionError:
             div_result = 'NaN'
-            logger.error("Divide by zero error. Could not divide %d by %d.", DIV_1, DIV_2,
-                         extra={"tags": {"service": "main"}})
+            logger.error(f"Divide by zero error, cannot divide {DIV_1} by {DIV_2}")
 
         alert_email = EmailUsers('jared.paquette@baycollege.edu', 'test.repo@baycollege.edu')
         alert_email.create_subject("Test Template Repo")
         alert_message = f"""
-        The results of our math operations.
+            The results of our math operations.
 
-        ADD: {add_result}
-        SUB: {sub_result}
-        MULT: {mult_result}
-        DIV: {div_result}
-        """
+            ADD: {add_result}
+            SUB: {sub_result}
+            MULT: {mult_result}
+            DIV: {div_result}
+            """
         alert_email.create_message(alert_message)
         alert_email.send_email()
 
---
+++ test_template/logging/datadog_logger.py
@@ -0,0 +1,101 @@
+import json
+import logging
+import os
+import sys
+
+from datadog_api_client import ApiClient, Configuration
+from datadog_api_client.v2.api.logs_api import LogsApi
+from datadog_api_client.v2.model.content_encoding import ContentEncoding
+from datadog_api_client.v2.model.http_log import HTTPLog
+from datadog_api_client.v2.model.http_log_item import HTTPLogItem
+from dotenv import load_dotenv
+
+# load in the .env file
+load_dotenv('config.env')
+# Set up the Datadog API client
+config = Configuration()
+config.api_key['DD-API-KEY'] = os.getenv("DD_API_KEY")
+print('DD_API_KEY: ', os.getenv("DD_API_KEY"))
+config.host = 'https://http-intake.logs.us5.datadoghq.com'
+api_client = ApiClient(configuration=config)
+logs = LogsApi(api_client)
+
+# Set up logging to file and terminal
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s [%(process)d] [%(levelname)s] %(message)s",
+    handlers=[
+        logging.FileHandler(filename="./app.log", mode="w"),
+        logging.StreamHandler(sys.stdout)
+    ]
+)
+
+# # Set up the logger for Uvicorn to use
+# logger = logging.getLogger("uvicorn")
+# logger.handlers = logging.getLogger().handlers
+# logger.info('Starting the app logging...')
+
+# Define a custom logging handler that sends logs to Datadog
+
+
+class DatadogHandler(logging.Handler):
+    def emit(self, record):
+        # # Ignore debug messages
+        # if record.levelno == logging.DEBUG:
+        #     return
+
+        toJson = json.dumps(
+            {
+                "python-logging": {
+                    "py-env": os.getenv("DD_LOGGING_ENV"),
+                    "py-message": record.getMessage(),
+                    "py-status": record.levelname.lower(),
+                    "py-logger": record.name,
+                    "py-stacktrace": record.exc_info,
+                    "py-exception": record.exc_text,
+                    "py-line": record.lineno,
+                    "py-file": record.filename,
+                    "py-function": record.funcName,
+                    "py-level": record.levelno,
+                    "py-path": record.pathname,
+                    "py-thread": record.thread,
+                    "py-threadName": record.threadName,
+                    "py-process": record.process,
+                    "py-processName": record.processName,
+                    "py-args": record.args,
+                    "py-msecs": record.msecs,
+                    "py-relativeCreated": record.relativeCreated,
+                    "py-created": record.created,
+                    "py-module": record.module,
+                }
+            }
+
+        )
+
+        # Send the log to Datadog using the Logs API
+        try:
+            body = HTTPLog(
+                [
+                    HTTPLogItem(
+                        ddsource="Python",
+                        ddtags="env:{}".format(os.getenv("DD_LOGGING_ENV")),
+                        hostname="{}".format(os.getenv("DD_HOSTNAME")),
+                        message=toJson,
+                        service="{}".format(os.getenv("DD_SERVICE")),
+                        status=record.levelname.lower()
+                    ),
+                ]
+            )
+
+            logs.submit_log(content_encoding=ContentEncoding.DEFLATE, body=body)
+            # print(response)
+
+        except Exception as e:
+            print(f"Error sending log to Datadog: {e}")
+
+
+# Set up the logger for Uvicorn to use
+logger = logging.getLogger("test-template")
+# logger.handlers = logging.getLogger().handlers
+logger.info('Starting the app logging...')
+logger.addHandler(DatadogHandler())
